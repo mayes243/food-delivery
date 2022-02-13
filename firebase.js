56c6472fdd45b866a43953fb54b478bd2947cda8
mayeskhatri243 <mayeskhatri243@gmail.com>
2022-02-13 15:34:45 +0545

    some changes

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0PPtD-R6NoCPr0rxHGG6M8-J5Pv1SWMw",
-  authDomain: "food-delivery-e53e2.firebaseapp.com",
-  projectId: "food-delivery-e53e2",
-  storageBucket: "food-delivery-e53e2.appspot.com",
-  messagingSenderId: "341825324483",
-  appId: "1:341825324483:web:31a64a370272b3d727a976",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
